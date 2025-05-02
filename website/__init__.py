103685b7370f9f856abba262e38d62e2ce0d91c2
WCyber <whdev36@gmail.com>
2025-05-02 16:42:54 +0500

    feat: update requirements and initialize SQLAlchemy in the Flask app

--- website/__init__.py
+++ website/__init__.py
@@ -1,12 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 import os
 import dotenv
 dotenv.load_dotenv()
 
+db = SQLAlchemy()
+
 def create_app():
     app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/')
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///website.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+    db.init_app(app)
     
     from . import views
     from . import auth
