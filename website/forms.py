5929d2ec2d0c2101ea9621c32e4887d3321c390f
WCyber <whdev36@gmail.com>
2025-05-03 09:09:04 +0500

    feat: add registration form with username and email validation

--- website/forms.py
+++ website/forms.py
@@ -1,9 +1,27 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField, BooleanField, PasswordField, EmailField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from .models import User
 
 class LoginForm(FlaskForm):
     email = EmailField('Email Address', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=8)])
     remember = BooleanField('Remember me', validators=[DataRequired()])
     submit = SubmitField('Login')
+
+class RegisterForm(FlaskForm):
+    name = StringField('Your Name', validators=[DataRequired()])
+    email = EmailField('Email Address', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8)])
+    password_confirm = PasswordField('Password Confirm', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username).first()
+        if user:
+            raise ValidationError('Username already exists. Please choose a different one.')
+        
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email).first()
+        if user:
+            raise ValidationError('Email already exists. Please choose a different one.')
