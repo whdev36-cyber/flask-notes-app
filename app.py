300c475cb1af2cd0dcd86a435f8d0c5d90858ebb
WCyber <whdev36@gmail.com>
2025-05-18 11:33:14 +0500

    feat: implement rendering of index.html template in Flask app

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template as render
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 @app.route('/home')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5555, host='0.0.0.0')
