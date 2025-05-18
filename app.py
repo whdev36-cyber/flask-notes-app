314beab097e329a3c50ac39fb8c7e67ce973279f
WCyber <whdev36@gmail.com>
2025-05-18 11:42:03 +0500

    feat: implement note rendering in index.html and add sample notes data

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template as render
+import json
+import os
 
 app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 @app.route('/home')
 def index():
-    return render('index.html')
+    with open('data/notes.json', 'r') as f:
+        notes = json.load(f)
+    return render('index.html', notes=notes)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5555, host='0.0.0.0')
