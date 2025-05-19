e4d61278503a6092f1bb275acb37d7b89f83aa85
WCyber <whdev36@gmail.com>
2025-05-19 09:11:01 +0500

    fix: add missing relationship for notes in User model

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), nullable=False)
     password = db.Column(db.String(255), nullable=False)
+    notes = db.relationship('Note', backref='author', lazy=True)
 
     def __repr__(self):
         return self.email
@@ -32,6 +33,7 @@ class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(150), nullable=False, default=lambda: dt.now().strftime("%Y-%m-%d %H:%M:%S"))
     content = db.Column(db.Text(), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', nullable=False))
 
     def __repr__(self):
         return self.title
