148edd28969636c05af38fe2200533dcdc891c6c
WCyber <whdev36@gmail.com>
2025-05-19 09:12:41 +0500

    fix: correct foreign key definition for user_id in Note model

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(150), nullable=False, default=lambda: dt.now().strftime("%Y-%m-%d %H:%M:%S"))
     content = db.Column(db.Text(), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id', nullable=False))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
         return self.title
